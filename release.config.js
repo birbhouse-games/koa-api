2016eaf91b24b65508cc608309609162ef3641e7
Trezy <tre@trezy.com>
2025-12-19 09:42:03 -0600

    feat: remove Yarn in favor of Bun, update dependencies, and optimize semantic-release

--- release.config.js
+++ release.config.js
@@ -1,21 +1,9 @@
 export default {
 	branches: [
-		'main',
+		"main",
 		{
-			name: 'beta',
+			name: "beta",
 			prerelease: true,
 		},
 	],
-	plugins: [
-		'@semantic-release/commit-analyzer',
-		'@semantic-release/release-notes-generator',
-    ["@semantic-release/npm", {
-			"npmPublish": false,
-			"tarballDir": "dist"
-		}],
-		['@semantic-release/github', {
-			"assets": "dist/*.tgz"
-		}],
-		'semantic-release-export-data',
-	],
-}
+};
